40c4f66c977ca1b16ddefa7c3580f1ca2bb2b113
VoteVeto2 <xinyu.yang@student.kuleuven.be>
2025-05-18 18:45:32 +0200

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EUResearchNetworkDashboard from './components/EUResearchNetworkDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <EUResearchNetworkDashboard />
+    </div>
+  );
+}
+
+export default App;
